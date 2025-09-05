dc77d98a4f75721534acbd873a2403fad5213898
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-05 18:31:21 -0300

    feat: 🚧 add rabbitmq topology

--- aws/k8s/secret.sh
+++ aws/k8s/secret.sh
@@ -2,3 +2,8 @@ kubectl create secret generic fc-hrsc-db-secret \
   --from-literal=username=root \
   --from-literal=password=root \
   -n codeflix
+
+kubectl create secret generic fc-hrsc-rabbitmq-secret \
+  --from-literal=username=adm_videos \
+  --from-literal=password=123456 \
+  -n codeflix
