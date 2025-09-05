fcb14a7c7bfe94d8399a81af432f9bf8ef55d451
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-05 20:44:19 -0300

    feat: ✨ add region to providers

--- aws/terraform/cluster/providers.tf
+++ aws/terraform/cluster/providers.tf
@@ -9,6 +9,7 @@ terraform {
   }
 
   backend "s3" {
+    region         = "us-east-2"
     bucket         = "codeflix-terraform"
     key            = "states/terraform.cluster.tfstate"
     dynamodb_table = "tf-state-locking"
